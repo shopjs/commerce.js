16e1aa009fe95b5a7e781177c81ebfb47861490e
David Tai <david@verus.io>
2020-02-07 16:36:15 -0600

    basic tests

--- src/Commerce.test.ts
+++ src/Commerce.test.ts
@@ -6,12 +6,36 @@ const KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NzIzMDc4NDcsInN1Yi
 const ENDPOINT = 'https://api-dot-hanzo-staging-249116.appspot.com/'
 
 describe('Commerce', () => {
-  test('default constructor', () => {
-    let c = new Commerce(new Api({
-      key: KEY,
-      endpoint: ENDPOINT,
-    }))
+  let client = new Api({
+    key: KEY,
+    endpoint: ENDPOINT,
+  })
 
-    expect(c).not.toBeUndefined()
+  test('default constructor', () => {
+    let order = {
+      currency: 'usd',
+      items: [],
+    }
+
+    let c = new Commerce(client, order)
+
+    expect(c.client).toBe(client)
+    expect(c.order.currency).toBe(order.currency)
+    expect(c.order.type).toBe('stripe')
+    expect(c.order.storeId).toBe('')
+    expect(c.order.items).toEqual([])
+  })
+
+  test('default cartInit', async () => {
+    let order = {
+      currency: 'usd',
+      items: [],
+    }
+
+    let c = new Commerce(client, order)
+    let cart = await c.cartInit()
+
+    expect(cart.id).toEqual(expect.any(String))
+    expect(c.cart.id).toBe(cart.id)
   })
 })
